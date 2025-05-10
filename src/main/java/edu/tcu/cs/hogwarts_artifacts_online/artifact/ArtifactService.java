7e2130c55f8bf3830646c7d37285b93e521dcd7a
tiartrop <90901624+tiartrop@users.noreply.github.com>
2025-05-11 03:17:50 +0800

    Add observability stack (Prometheus, Grafana, and Zipkin) to the project.

--- src/main/java/edu/tcu/cs/hogwarts_artifacts_online/artifact/ArtifactService.java
+++ src/main/java/edu/tcu/cs/hogwarts_artifacts_online/artifact/ArtifactService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import edu.tcu.cs.hogwarts_artifacts_online.artifact.utils.IdWorker;
 import edu.tcu.cs.hogwarts_artifacts_online.system.exception.ObjectNotFoundException;
 import io.micrometer.core.annotation.Timed;
+import io.micrometer.observation.annotation.Observed;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -22,6 +23,7 @@ public class ArtifactService {
     this.idWorker = idWorker;
   }
 
+  @Observed(name = "artifact", contextualName = "findByIdService")
   public Artifact findById(String artifactId) {
     return this.artifactRepository.findById(artifactId).orElseThrow(() -> new ObjectNotFoundException("artifact", artifactId));
   }
--- src/main/java/edu/tcu/cs/hogwarts_artifacts_online/security/SecurityConfiguration.java
+++ src/main/java/edu/tcu/cs/hogwarts_artifacts_online/security/SecurityConfiguration.java
@@ -70,8 +70,8 @@ public class SecurityConfiguration {
               .requestMatchers(HttpMethod.POST, this.baseUrl + "/users").hasAnyAuthority("ROLE_admin")
               .requestMatchers(HttpMethod.PUT, this.baseUrl + "/users/**").hasAnyAuthority("ROLE_admin")
               .requestMatchers(HttpMethod.DELETE, this.baseUrl + "/users/**").hasAnyAuthority("ROLE_admin")
-              .requestMatchers(EndpointRequest.to("health", "info")).permitAll()
-              .requestMatchers(EndpointRequest.toAnyEndpoint().excluding("health", "info")).hasAnyAuthority("ROLE_admin")
+              .requestMatchers(EndpointRequest.to("health", "info", "prometheus")).permitAll()
+              .requestMatchers(EndpointRequest.toAnyEndpoint().excluding("health", "info", "prometheus")).hasAnyAuthority("ROLE_admin")
               .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
               // Disallow everything else.
               .anyRequest().authenticated() // Always a good idea to put this as last.
